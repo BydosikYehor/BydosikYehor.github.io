fc692b9b6190c7a03ee46af0d76e8a529bb509c5
Yehor <bydosik@gmail.com>
2022-04-17 17:51:08 +0300

    First commit

---
+++ java.js
@@ -0,0 +1,9 @@
+﻿function showText (el) {
+    if (el.previousElementSibling.clientHeight 
+        el.previousElementSibling.style.height = "40%";=== 80) {
+        el.innerHTML = "Show Less...";      
+    } else {
+        el.previousElementSibling.style.height = "80px";
+        el.innerHTML = "Read More...";
+    }
+}
